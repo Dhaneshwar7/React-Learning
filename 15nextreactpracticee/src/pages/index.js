e3d47970fcfe159ce623ad153f195bba8f4bf024
Delta Mike <87529812+DELTAMIKE7@users.noreply.github.com>
2024-07-04 23:30:38 +0530

    lifecycle methods

--- 15nextreactpracticee/src/pages/index.js
+++ 15nextreactpracticee/src/pages/index.js
@@ -2,18 +2,41 @@ import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import ClickCounter from '@/components/ClickCounter';
 import HoverCounter from '@/components/HoverCounter';
-
+import { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+	const [so, setSo] = useState(null);
+
+	const styl = {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'center',
+	};
+  useEffect(() => {
+
+    // setTimeout(() => {
+    //   setSo("Entered")
+    // }, 3000);
+    console.log("yooo runnning");
+    console.log(so);
+
+    return ()=>{
+      console.log("unmouunted bc");
+    }
+   
+  })
+  
+
 	return (
 		<>
-    <div style={{display:"flex",flexDirection:"column" , alignItems:"center",justifyContent:"center"}}>
-
-			<HoverCounter />
-      <ClickCounter/>
-    </div>
+			<div style={styl}>
+				<div> {so ? so : <div>you runn</div>}</div>
+				<HoverCounter />
+				<ClickCounter />
+			</div>
 		</>
 	);
 }
